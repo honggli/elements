6f428d3380a6aa9f34b3e0a19df85a417c6bc0a7
honggli <honggangli722@gmail.com>
2013-06-20 01:04:38 +0800

    Update element.rb

--- element.rb
+++ element.rb
@@ -1,4 +1,8 @@
 module UTILS
   class ELEMENT
+    # when instantiate an object of ELEMENT or its subclass, set each field to the default value if supplied
+    def initialize
+      @@fields.each { |field| instance_variable_set("@#{field[:name]}", field[:default]) if field[:default] }
+    end
   end
 end
