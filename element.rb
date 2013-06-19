55e092de7e62693df3132dd57e6d99d1bd7f2680
honggli <honggangli722@gmail.com>
2013-06-20 02:10:19 +0800

    update comment of initialize

--- element.rb
+++ element.rb
@@ -1,6 +1,6 @@
 module UTILS
   class ELEMENT
-    # when instantiate an object of ELEMENT or its subclass, set each field to the default value if supplied
+    # define instance method initialize(), which is used to initialize the object of ELEMENT or its subclass
     def initialize
       @@fields.each { |field| instance_variable_set("@#{field[:name]}", field[:default]) if field[:default] }
     end
