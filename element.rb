7b411d70c6f007c537af2fc7e8e81a85130ebbf8
honggli <honggangli722@gmail.com>
2013-06-20 22:28:59 +0800

    Update element.rb
    add decode()

--- element.rb
+++ element.rb
@@ -4,5 +4,10 @@ module UTILS
     def initialize
       @@fields.each { |field| instance_variable_set("@#{field[:name]}", field[:default]) if field[:default] }
     end
+    
+    # define instance method decode() to decode the byte stream into the object
+    def decode(stream)
+      ("0x"+stream).hex.to_s(2)
+    end
   end
 end
